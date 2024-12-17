937418653102c1fedb6b8474b0c16469498c1d5a
cinna03 <c.mwiza1@alustudent.com>
2024-12-17 19:19:17 +0200

    commit

--- ideas/script.js
+++
@@ -1,8 +0,0 @@
-// Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("alertButton");
-
-  button.addEventListener("click", () => {
-    alert("Hello! Welcome to my first website.");
-  });
-});
